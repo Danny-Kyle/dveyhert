be0f5dcc1f83a6f51d63d06e5e64e7257b26b38d
Danny-Kyle <dikedaniel7917@gmail.com>
2024-01-25 15:06:03 +0100

    sales trend

--- app/components/SalesTrends.js
+++ app/components/SalesTrends.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const SalesTrends = () => {
   return (
-    <div>SalesTrends</div>
+    <main>
+      <section>
+        <div>
+          <h2>Sales Trends</h2>
+          <div>Sort by: 
+            <div className='border border-solid rounded-full flex flex-row gap-2 w-[96px] h-[32px] items-center justify-center'>Weekly
+            <Image src="/arrowdown.svg" alt='arrowdown' width={5} height={5} />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section></section>
+    </main>
   )
 }
 
