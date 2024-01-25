013e1f6abeadf4838e2424ca1ac8f09bde44b06f
Danny-Kyle <dikedaniel7917@gmail.com>
2024-01-25 12:17:48 +0100

    newday

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div><Image src={"/calendar.svg"} alt="img" width="20" height="10"/></div>
         November 15, 2023
       </div>
-      <div className='border border-solid border-gray-400 rounded-full p-3 w-12 h-12 justify-center items-center items-center'>
+      <div className='border border-solid border-gray-400 rounded-full p-3 w-12 h-12 justify-center cursor-pointer items-center'>
         <Image src={"/notification.svg"} alt="img" width="17" height="18"/>
       </div>
       {/* <div className='flex flex-col  rounded-full border border-solid border-gray-400 p-3'>
@@ -27,10 +27,10 @@ const Navbar = () => {
         <div>
           {fname}@gmail.com
         </div>
-        <Image src={"/arrowdown.svg"} alt="img" width="14" height="8" />
       </div> */}
-        <div className='flex flex-col  rounded-full border border-solid border-gray-400 w-48 h-24'></div>
-        
+        <div className='flex flex-col rounded-full border border-solid border-gray-400 w-48 h-24'>
+            <Image src={"/arrowdown.svg"} alt="img" width="14" height="8" />
+        </div>
       </div>
     </div>
   )
