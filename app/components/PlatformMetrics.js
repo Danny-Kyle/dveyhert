1d7a8c567b3110be02fabaa9a4e3ed771f181bb0
Danny-Kyle <dikedaniel7917@gmail.com>
2024-01-25 13:48:00 +0100

    plats

--- app/components/PlatformMetrics.js
+++ app/components/PlatformMetrics.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const PlatformMetrics = () => {
   return (
-    <div>PlatformMetrics</div>
+    <section className='w-[488px] h-[542px] bg-white rounded-2xl'>
+      <div className='bg-white rounded-2xl p-3'>
+        <header className='flex flex-row justify-between mb-4'>
+        <h2 className='font-medium'>Top Platforms</h2>
+        <h2 className='font-semibold text-green-500'>See All</h2>
+        </header>
+        </div>
+    </section>
   )
 }
 
