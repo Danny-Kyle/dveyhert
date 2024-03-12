79adaf5d8bd3b3c1820a205b638b582e80ac54a8
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-12 20:17:00 +0100

    modal made

--- app/components/PlatformMetrics.js
+++ app/components/PlatformMetrics.js
@@ -4,11 +4,11 @@ import platforms from "./platforms.json"
 
 const PlatformMetrics = () => {
   return (
-    <section className='w-[488px] h-[542px] bg-white rounded-2xl'>
+    <section className='w-[488px] h-[542px] bg-white rounded-2xl text-black'>
       <div className='bg-white rounded-2xl p-3'>
         <header className='flex flex-row justify-between mb-4'>
         <h2 className='font-medium'>Top Platforms</h2>
-        <h2 className='font-semibold text-green-500'>See All</h2>
+        <h2 className='font-semibold text-green-500 cursor-pointer'>See All</h2>
         </header>
         {platforms.map((order, index) => (
         <div key={index} className='border-b-2 my-2 py-2 grid grid-rows-3'>
--- app/components/SalesTrends.js
+++ app/components/SalesTrends.js
@@ -1,14 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
 
+function Modal(){
+  const Objs = ["Daily", "Weekly", "Monthly", "Yearly"];
+  return (
+    Objs.map((item, index) => (
+      <ul key={index}>
+        <li>{item}</li>
+      </ul>
+    ))
+    
+  )
+}
+
+
 const SalesTrends = () => {
+  const [time, setTime] = useState(true)
+  function toggleTime(){
+    setTime(!time)
+  }
   return (
     <main>
       <section>
-        <div className='flex flex-row justify-between'> 
+        <div className='flex flex-row justify-between bg-white text-black rounded-full  p-4 my-3'> 
           <h2>Sales Trends</h2>
           <div>Sort by: 
-            <div className='border border-solid rounded-full flex flex-row gap-2 w-[96px] h-[32px] items-center justify-center'>Weekly
+            <div onClick={toggleTime} className='border border-solid cursor-pointer rounded-full flex flex-row gap-2 w-[96px] h-[32px] items-center justify-center'>
+              {}
+            {time && Modal()}
             <Image src="/arrowdown.svg" alt='arrowdown' width={5} height={5} />
             </div>
           </div>
